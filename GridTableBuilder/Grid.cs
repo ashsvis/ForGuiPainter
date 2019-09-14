cdb2afe3261de2fd51a0df388b14fb4f2060ce83
ashsvis <ashsvis@gmail.com>
2019-09-14 09:19:26 +0400

    Найден алгоритм для проверки и переработки

--- GridTableBuilder/Grid.cs
+++ GridTableBuilder/Grid.cs
@@ -72,6 +72,165 @@ namespace GridTableBuilder
             WorkMode = GridWorkMode.Draw;
         }
 
+        /*
+         * http://www.cyberforum.ru/algorithms/thread2008601.html#post10576549
+         * 
+var np:integer;  // число узлов графа
+var xa,ya:array [0..100] of integer;  // координаты узлов
+adj:array [0..100,0..100] of integer; // матрица сопряжений
+stack,visited:array [0..100] of integer; // стек для поиска в глубину
+seg:array [0..1,0..100] of integer;nseg:integer; // массив ребер графа
+ 
+type TCircle=record
+             els:array [0..100] of integer;
+             nel:integer;
+             end;
+var circles:array [0..100] of TCircle; // массив найденных циклов графа
+ncl:integer; // число найденных циклов графа
+t:integer; // переменная стека
+ 
+function CompareCircles(i1,i2:integer):Boolean;
+var i,j,k:integer;
+begin
+Result:=False;
+if circles[i1].nel<>circles[i2].nel then exit;
+k:=0;
+for i:=0 to circles[i1].nel-2 do
+   begin
+   for j:=0 to circles[i2].nel-2 do
+      begin
+      if circles[i1].els[i]=circles[i2].els[j] then k:=k+1;
+      end;
+   end;
+if k=(circles[i1].nel-1) then Result:=True;
+end;
+ 
+function rDirAngle(x1,y1,x2,y2:Extended):Extended;
+var x:Extended;
+begin
+if Abs(y2-y1)<0.000001 then
+   begin
+   if x2>x1 then begin Result:=PI/2;exit;end;
+   if x2<x1 then begin Result:=3*PI/2;exit;end
+   end;
+x:=Arctan(Abs(x2-x1)/Abs(y2-y1))*180/PI;
+if (x2>=x1) and (y2>y1) then Result:=x;
+if (x2>=x1) and (y2<y1) then Result:=180-x;
+if (x2<=x1) and (y2<y1) then Result:=180+x;
+if (x2<=x1) and (y2>y1) then Result:=360-x;
+Result:=Result*PI/180;
+end;
+ 
+function IsHorderCircle(n:integer):Boolean;
+var i,j,d,k,i1,i2:integer;
+a1,a2,a3:double;
+begin
+d:=0;
+for i:=0 to circles[n].nel-2 do
+   begin
+   d:=d+xa[circles[n].els[i]]*ya[circles[n].els[i+1]]-xa[circles[n].els[i+1]]*ya[circles[n].els[i]];
+   end;
+d:=sign(d); // -1 - против, =1 - по  (или наоборот)
+//---------Если против часовой стрелки, поменять направление
+if d<0 then
+   begin
+   for i:=0 to circles[n].nel div 2 - 1 do
+      begin
+      k:=circles[n].els[i];
+      circles[n].els[i]:=circles[n].els[circles[n].nel-1-i];
+      circles[n].els[circles[n].nel-1-i]:=k;
+      end;
+   end;
+//---------Обход по часовой, проверка ответвлений вправо
+circles[n].els[circles[n].nel]:=circles[n].els[1];  //--Добавляем ещё один элемент в цикл
+for i:=0 to circles[n].nel-2 do
+   begin
+   for j:=0 to np-1 do  //---поиск сопряжённых точек
+      begin
+      if (adj[circles[n].els[i+1],j]=1) and (j<>circles[n].els[i]) and (j<>circles[n].els[i+1]) and (j<>circles[n].els[i+2]) then
+         begin
+         a1:=rDirAngle(xa[circles[n].els[i+1]],ya[circles[n].els[i+1]],xa[circles[n].els[i]],ya[circles[n].els[i]]);
+         a2:=rDirAngle(xa[circles[n].els[i+1]],ya[circles[n].els[i+1]],xa[circles[n].els[i+2]],ya[circles[n].els[i+2]]);
+         a3:=rDirAngle(xa[circles[n].els[i+1]],ya[circles[n].els[i+1]],xa[j],ya[j]);
+         a1:=a1*180/PI;a2:=a2*180/PI;a3:=a3*180/PI;
+         a1:=a1-a2; while (a1<360) do a1:=a1+360;while (a1>360) do a1:=a1-360;
+         a3:=a3-a2; while (a3<360) do a3:=a3+360;while (a3>360) do a3:=a3-360;
+         if a3>a1 then begin Result:=true;exit;end;
+         end;
+      end;
+   end;
+IntToStr(n);
+Result:=False;
+end;
+ 
+procedure push(k:integer);
+begin
+stack[t]:=k;t:=t+1;
+end;
+ 
+procedure pop(k:integer);
+begin
+t:=t-1;
+end;
+ 
+procedure DFS(k,v:integer);
+var i,j,m,i1,j1:integer;
+begin
+push(k);
+visited[k]:=1;
+for i:=0 to np-1 do
+    begin
+    if i=k then continue;
+    if i=v then continue;
+    if (visited[i]=1) and (adj[i,k]<>0) then //--если не сама с себя
+       begin
+       for j:=0 to t-1 do
+          begin
+          circles[ncl].els[j]:=stack[j];
+          end;
+       for j:=0 to t-1 do if stack[j]=i then break;
+       for m:=j to t-1 do circles[ncl].els[m-j]:=stack[m]; //---переносим цикл из стека
+       circles[ncl].els[t-j]:=i;
+       circles[ncl].nel:=t-j+1;
+//-------------проверка, а может такой цикл уже есть?
+       j1:=0;
+       for i1:=0 to ncl-1 do
+          begin
+          if CompareCircles(i1,ncl)=True then begin j1:=1;break;end;
+          end;
+//-------------проверка, цикл с внутренними хордами?
+       if IsHorderCircle(ncl)=True then j1:=1;
+       if j1=0 then ncl:=ncl+1; //--Если такого цикла не было, то добавляем, иначе - нет
+       continue;
+       end;
+    if adj[i,k]<>0 then DFS(i,k);
+    end;
+visited[k]:=0;
+pop(k);
+end;
+ 
+procedure TForm13.Button2Click(Sender: TObject);
+var i,j,k,minx,maxx:integer;
+begin
+//----------Сформировать adj
+for i:=0 to 100 do for j:=0 to 100 do adj[i,j]:=0;
+for i:=0 to nseg-1 do
+   begin
+   adj[seg[0,i],seg[1,i]]:=1;
+   adj[seg[1,i],seg[0,i]]:=1;
+   end;
+//----------Запустить DFS
+t:=0;ncl:=0;
+for i:=0 to np-1 do visited[i]:=0;
+for i:=0 to np-1 do
+   begin
+   if visited[i]=0 then DFS(i,-1);
+   end;
+minx:=100000;maxx:=-100000;for i:=0 to np-1 do begin if xa[i]<minx then minx:=xa[i];if xa[i]>maxx then maxx:=xa[i];end;
+for i:=0 to ncl-1 do begin Draw(i*(maxx-minx+30),300);DrawCircles(i,i*(maxx-minx+30),300);end;
+end
+         */
+
         List<PointNode> V = new List<PointNode>();
         List<Edge> E = new List<Edge>();
 
